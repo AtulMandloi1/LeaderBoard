7a3be20bbac33fbbb7b07facba76a2e3da49d6c0
rohith kumar reddy <atulmandloi16@gmail.com>
2024-07-25 23:48:42 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import logo from './logo.svg';
+import './App.css';
 import React from 'react';
 import Headers from './compoment/Headers'
 import Name from './compoment/Name';
@@ -5,17 +7,32 @@ import Mainbox from './compoment/Mainbox';
 import Baner from './compoment/Baner';
 import Footer from './compoment/Footer';
 
-const App = () => {
-
+function App() {
   return (
-    <div style={{height:'100vh'}}>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div style={{ height: '100vh' }}>
       <Headers />
       <Name />
       <Mainbox />
       <Baner />
-      <Footer/>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
